8af6d0f33a22bf134d7a6680c6bf33d18ef2f1d8
sivaveduru <sivaveduru@gmail.com>
2025-02-11 12:58:23 -0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,10 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
